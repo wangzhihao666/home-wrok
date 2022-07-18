97d90e1f5a376353c6fe5ed0d906bd8e2c75e2bd
wangzhihao <wzh18837360543@163.com>
2022-07-18 21:06:08 +0800

    file:封装button按钮可复用组件

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <wangButton>默认按钮</wangButton>
+    <wangButton type="primary"  >成功按钮</wangButton>
+    <wangButton type="danger" >危险按钮</wangButton>
+    <wangButton type="warning" >警告按钮</wangButton>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      flag: false
+    }
+  },
+  components: {
+    wangButton: () => import('../components/button/index.vue')
+  },
+  methods: {
+    handleSubmit () {
+      this.flag = true
+      setTimeout(() => {
+        console.log('666')
+        this.flag = false
+      }, 3000)
+    }
+  }
+}
+</script>
